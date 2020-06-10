f2558fadb9eafe3d6ac6ca94653d92af464d178f
Christopher Mohammed <christophermohammed11@outlook.com>
2020-06-09 23:13:56 -0400

    added router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ThemeContextProvider from 'context/theme/provider';
 import ThemeProvider from 'theme/themeProvider';
-import Page from 'components/Page';
-import ThemeToggle from 'components/buttons/ThemeToggle';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from 'pages/Home';
+import Membership from 'pages/Membership';
+import { ROUTES } from 'utils/constants';
 
 function App() {
   return (
     <ThemeContextProvider>
       <ThemeProvider>
-        <Page>
-          <ThemeToggle />
-        </Page>
+        <BrowserRouter>
+          <Route exact path={ROUTES.HOME} component={Home} />
+          <Route path={ROUTES.MEMBERSHIP} component={Membership} />
+        </BrowserRouter>
       </ThemeProvider>
     </ThemeContextProvider>
   );
---
+++ client/src/pages/Membership/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from 'components/Page';
+
+function Membership() {
+  return (
+    <Page>
+      <h1>Membership</h1>
+    </Page>
+  );
+}
+
+export default Membership;
