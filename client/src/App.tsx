872af46f11a4542b5dd2971ef21ee188b72c3fce
Christopher Mohammed <christophermohammed11@outlook.com>
2020-06-10 16:59:13 -0400

    added switch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ThemeContextProvider from 'context/theme/provider';
 import ThemeProvider from 'theme/themeProvider';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import Membership from 'pages/Membership';
 import { ROUTES } from 'utils/constants';
@@ -11,8 +11,10 @@ function App() {
     <ThemeContextProvider>
       <ThemeProvider>
         <BrowserRouter>
-          <Route exact path={ROUTES.HOME} component={Home} />
-          <Route path={ROUTES.MEMBERSHIP} component={Membership} />
+          <Switch>
+            <Route exact path={ROUTES.HOME} component={Home} />
+            <Route path={ROUTES.MEMBERSHIP} component={Membership} />
+          </Switch>
         </BrowserRouter>
       </ThemeProvider>
     </ThemeContextProvider>
