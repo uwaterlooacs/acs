e5c7acd08b3c23f2b95302a76fb1d96b34842d4c
Joshua Allum <joshuatallum@gmail.com>
2020-06-13 12:16:09 -0400

    Remove EVENT_NAMES array

--- src/routers/mailingList/constant.ts
+++ src/routers/mailingList/constant.ts
@@ -1,20 +1,13 @@
-export const EVENT_NAMES = [
-  'Games Night',
-  'Movie Night',
-  'Online Party',
-  'Scavenger Hunt',
-  'Talent Show',
-  'Study Buddies',
-  'Lewwe Talk',
-  'Trivia Night',
-  'Workout Sessions',
-  'Dance Sessions',
-];
-
 // Object with event names as keys and all values set to false
-export const DEFAULT_EVENT_RESPONSES = EVENT_NAMES.reduce<
-  Record<string, boolean>
->((partial, eventName) => {
-  partial[eventName] = false;
-  return partial;
-}, {});
+export const DEFAULT_EVENT_RESPONSES = {
+  'Games Night': false,
+  'Movie Night': false,
+  'Online Party': false,
+  'Scavenger Hunt': false,
+  'Talent Show': false,
+  'Study Buddies': false,
+  'Lewwe Talk': false,
+  'Trivia Night': false,
+  'Workout Sessions': false,
+  'Dance Sessions': false,
+};
--- src/routers/mailingList/types.ts
+++ src/routers/mailingList/types.ts
@@ -1,5 +1,7 @@
+import { DEFAULT_EVENT_RESPONSES } from './constant';
+
 export type MailingListRequestBody = {
   email: string;
-  interestedEvents: string[];
+  interestedEvents: (keyof typeof DEFAULT_EVENT_RESPONSES)[];
   otherFeedback: string;
 };
