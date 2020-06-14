9ebc482ea54fe28996e22b0526af2a52a3e67691
Christopher Mohammed <christophermohammed11@outlook.com>
2020-06-14 11:14:15 -0400

    mailinglistrequest

--- client/src/utils/data/mailingList.ts
+++ client/src/utils/data/mailingList.ts
@@ -1,11 +1,11 @@
-import { MailingListResponse } from 'types/mailingList';
+import { MailingListRequest } from 'types/mailingList';
 import { AuthResponse, Method } from 'types/network';
 import { makeRequest } from 'utils/network/request';
 import { APIRoutes } from 'utils/network/endpoints';
 import * as M from 'utils/network/errorMessages';
 
-export const mailingListSubmit = async (data: MailingListResponse) => {
-  return await makeRequest<AuthResponse, MailingListResponse>(
+export const mailingListSubmit = async (data: MailingListRequest) => {
+  return await makeRequest<AuthResponse, MailingListRequest>(
     Method.POST,
     APIRoutes.MAILING_LIST,
     M.MAILING_LIST,
