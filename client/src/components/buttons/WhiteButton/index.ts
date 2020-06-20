280c13296f84602f3e7aa8358b46523d0193667e
Joshua Allum <joshuatallum@gmail.com>
2020-06-19 21:30:22 -0400

    Base button colors on theme

--- client/src/components/buttons/WhiteButton/index.ts
+++ client/src/components/buttons/WhiteButton/index.ts
@@ -1,18 +1,19 @@
 import { Button, withStyles } from '@material-ui/core';
-import { common } from '@material-ui/core/colors';
 
 const ColorButton = withStyles((theme) => ({
   root: {
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
-    color: theme.palette.getContrastText(common.white),
-    backgroundColor: common.white,
+    color: theme.palette.getContrastText(theme.palette.background.default),
+    backgroundColor: theme.palette.background.default,
     border: '1px solid',
     borderRadius: 8,
-    borderColor: theme.palette.getContrastText(common.white),
+    borderColor: theme.palette.getContrastText(
+      theme.palette.background.default,
+    ),
     '&:hover, &.hover': {
-      color: theme.palette.getContrastText(common.black),
-      backgroundColor: common.black,
+      color: theme.palette.getContrastText(theme.palette.text.primary),
+      backgroundColor: theme.palette.text.primary,
     },
   },
 }))(Button);
