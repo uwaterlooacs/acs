ce868c4ab7f86ad3435d694eae93606dddc2d395
Christopher Krogh <chris.krogh@outlook.com>
2020-07-03 17:51:26 -0400

    removed cast

--- src/jobs/membership.ts
+++ src/jobs/membership.ts
@@ -1,10 +1,7 @@
 import cron from 'node-cron';
 import User from '../models/user';
 import { MEMBERSHIP_STATUS } from '../types/user';
-import {
-  GoogleSpreadsheet,
-  WorksheetBasicProperties,
-} from 'google-spreadsheet';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 import GoogleCreds from '../config/acs-web-5aaaf-768e669e2d39.json';
 
 const getTerm = () => {
@@ -29,7 +26,7 @@ cron.schedule('0 0 1 JAN,MAY,SEP *', async function () {
     await doc.loadInfo();
     const newSheet = await doc.addSheet({
       title: getTerm(),
-    } as WorksheetBasicProperties);
+    });
     const members = await User.find({
       $or: [
         { membershipStatus: MEMBERSHIP_STATUS.PAID },
