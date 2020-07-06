29f66f22ce99f7ff5ce1b51d447c5b874fb20250
Christopher Krogh <chris.krogh@outlook.com>
2020-07-06 17:38:47 -0400

    under construction

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import Page from 'components/Page';
-import MailingList from 'components/forms/MailingList';
+import { Typography } from '@material-ui/core';
 
 function Home() {
   return (
-    <Page>
-      <MailingList />
-    </Page>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <Typography variant="h1" align="center" color="textPrimary">
+        Hi there
+      </Typography>
+      <Typography variant="h4" align="center" color="textPrimary">
+        Our site is currently under construction
+      </Typography>
+      <Typography variant="h4" align="center" color="textPrimary">
+        Check back soon!
+      </Typography>
+    </div>
   );
 }
 
