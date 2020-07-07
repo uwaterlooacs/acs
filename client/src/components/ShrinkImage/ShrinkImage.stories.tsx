87906f7d1f77185623d4544469e62a6fb80c7713
Joshua Allum <joshuatallum@gmail.com>
2020-07-07 00:40:25 -0400

    Add ShrinkImage component
    - Animation makes image shrink from 1.25 larger size and optionally
    shift from 100px lower than normal to help with centering

---
+++ client/src/components/ShrinkImage/ShrinkImage.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ShrinkImage from '.';
+import BigArtWorkFlags from './BigArtworkFlags.png';
+import AreYouAMember from './AreYouAMember.png';
+
+export default {
+  component: ShrinkImage,
+  title: 'ShrinkImage',
+};
+
+export const FullSize = () => (
+  <ShrinkImage src={BigArtWorkFlags} alt="ACS Flags Artwork" />
+);
+
+export const InShortBlock = () => (
+  <div style={{ height: 300 }}>
+    <ShrinkImage src={BigArtWorkFlags} alt="ACS Flags Artwork" />
+  </div>
+);
+
+export const InRelativeVHBlock = () => (
+  <div style={{ height: '60vh' }}>
+    <ShrinkImage src={BigArtWorkFlags} alt="ACS Flags Artwork" />
+  </div>
+);
+
+export const WithShift = () => (
+  <div style={{ height: '30vh' }}>
+    <ShrinkImage src={AreYouAMember} alt="ACS Are you a Member?" shift />
+  </div>
+);
---
+++ client/src/components/ShrinkImage/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Transition } from 'react-transition-group';
+import { Theme, createStyles, WithStyles, withStyles } from '@material-ui/core';
+import classNames from 'classnames';
+
+type ShiftedClassName =
+  | 'entering-shift'
+  | 'entered-shift'
+  | 'exiting-shift'
+  | 'exited-shift'
+  | 'unmounted-shift';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    shrinkimage: {
+      display: 'block',
+      height: '100%',
+      maxWidth: '80vw',
+      margin: `${theme.spacing(4)}px auto`,
+      transition: 'transform 300ms ease-in-out',
+    },
+    entering: {
+      transform: 'scale(1.25)',
+    },
+    'entering-shift': {
+      transform: 'translateY(100px) scale(1.25)',
+    },
+    entered: {
+      transform: 'scale(1)',
+    },
+    'entered-shift': {
+      transform: 'translateY(0) scale(1)',
+    },
+    exiting: {},
+    'exiting-shift': {},
+    exited: {},
+    'exited-shift': {},
+    unmounted: {},
+    'unmounted-shift': {},
+  });
+
+interface Props
+  extends WithStyles<typeof styles>,
+    React.ImgHTMLAttributes<HTMLImageElement> {
+  shift?: boolean;
+}
+
+const ShrinkImage: React.FC<Props> = ({
+  classes,
+  shift,
+  ...imgProps
+}: Props) => {
+  return (
+    <Transition in appear timeout={1000}>
+      {(state) => (
+        <img
+          className={classNames(classes.shrinkimage, classes[state], {
+            [classes[`${state}-shift` as ShiftedClassName]]: shift,
+          })}
+          {...imgProps}
+        />
+      )}
+    </Transition>
+  );
+};
+
+export default withStyles(styles)(ShrinkImage);
