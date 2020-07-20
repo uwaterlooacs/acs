bb83eeae8755787ce5935350215b92fcea0beeb2
Joshua Allum <joshuatallum@gmail.com>
2020-07-20 02:41:07 -0400

    Bring client User types up to date with backend

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,14 +1,32 @@
 import { ModelMetadata } from './model';
 
+export enum MEMBERSHIP_STATUS {
+  EXPIRED = 'Not currently a Member',
+  UNPAID = 'Member has not paid',
+  PAID = 'Full Member',
+}
+
 export type Credentials = {
   email: string;
   password: string;
 };
 
 export type UserData = Omit<Credentials, 'password'> & {
-  name: string;
+  firstName: string;
+  lastName: string;
+  watIAMUserId: string;
+  studentNumber: number;
   picture?: string;
+  membershipStatus: MEMBERSHIP_STATUS;
+  isAdmin: string;
   password?: string;
 };
 
 export type User = UserData & ModelMetadata;
+
+export type SignUpUserData = Omit<
+  UserData & Credentials,
+  'membershipStatus' | 'isAdmin'
+> & {
+  paid?: boolean;
+};
