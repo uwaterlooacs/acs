db359d0ffabc2a8fc00a823d5ab70828db8563b1
Joshua Allum <joshuatallum@gmail.com>
2020-07-20 02:26:18 -0400

    Move student number validation to custom validator

--- src/routers/user/routeValidator.ts
+++ src/routers/user/routeValidator.ts
@@ -1,8 +1,7 @@
 import { check, oneOf, query, body } from 'express-validator';
-import { isPassword } from '../../utils/customValidators';
+import { isPassword, isStudentNumber } from '../../utils/customValidators';
 import { MEMBERSHIP_STATUS } from '../../types/user';
 
-const STUDENT_NUMBER_LENGTH = 8;
 const routeValidator = (route: string) => {
   switch (route) {
     case '/signup':
@@ -10,9 +9,7 @@ const routeValidator = (route: string) => {
         check('name').notEmpty().trim(),
         check('email').isEmail().normalizeEmail(),
         check('watIAMUserId').notEmpty().trim(),
-        check('studentNumber')
-          .isNumeric()
-          .isLength({ min: STUDENT_NUMBER_LENGTH, max: STUDENT_NUMBER_LENGTH }),
+        check('studentNumber').custom(isStudentNumber),
         check('password').custom(isPassword),
         check('paid').optional().isBoolean(),
         check('picture').optional().isString().trim(),
--- src/utils/customValidators.ts
+++ src/utils/customValidators.ts
@@ -1,13 +1,31 @@
+import validator from 'validator';
+
 const MIN_PASSWORD_LENGTH = 8;
 export const isPassword = (password?: string) => {
   if (!password) {
-    throw new Error('Password is required.');
+    throw new Error('password is required.');
   }
   if (password.length < MIN_PASSWORD_LENGTH) {
     throw new Error(
-      `Password is too short. Minimum length is ${MIN_PASSWORD_LENGTH} characters.`,
+      `password is too short. Minimum length is ${MIN_PASSWORD_LENGTH} characters.`,
     );
   }
   // we can add more password constraints here
+  return true;
+};
+
+const STUDENT_NUMBER_LENGTH = 8;
+export const isStudentNumber = (studentNumber?: string) => {
+  if (!studentNumber) {
+    throw new Error('studentNumber is required.');
+  }
+  if (studentNumber.length !== STUDENT_NUMBER_LENGTH) {
+    throw new Error(
+      `studentNumber is must be ${STUDENT_NUMBER_LENGTH} characters in length.`,
+    );
+  }
+  if (!validator.isNumeric(studentNumber)) {
+    throw new Error(`studentNumber is must only contain numeric characters.`);
+  }
   return true;
 };
