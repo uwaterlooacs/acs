9aa5827a05d131c39e0b7bd91f75e06758b1abff
jtaaa <joshuatallum@gmail.com>
2021-01-03 17:18:09 -0500

    Add initial NomineeCard

---
+++ packages/client/src/components/NomineeCard/index.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+  CardMedia,
+  makeStyles,
+  Box,
+} from '@material-ui/core';
+import { VOTING_STAGE } from '@acs/shared';
+
+const useStyles = makeStyles({
+  card: {
+    height: 550,
+  },
+  cardContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    overflow: 'auto',
+  },
+  video: {
+    maxHeight: 250,
+  },
+  writeUp: {
+    whiteSpace: 'pre-wrap',
+  },
+});
+
+export type Props = {
+  firstName: string;
+  lastName: string;
+  semester: string;
+  faculty: string;
+  videoUrl?: string;
+  writeUp?: string;
+  stage: VOTING_STAGE.Nomination | VOTING_STAGE.Vote;
+  isDisabled: boolean;
+  onPrimaryButtonClicked(): Promise<void>;
+};
+
+const NomineeCard: React.FC<Props> = ({
+  firstName,
+  lastName,
+  semester,
+  faculty,
+  videoUrl,
+  writeUp,
+  stage,
+  isDisabled,
+  onPrimaryButtonClicked,
+}) => {
+  const classes = useStyles();
+  const primaryButtonActionText =
+    stage === VOTING_STAGE.Nomination ? 'Second Nomination' : 'Vote';
+  const primaryButtonDisabledText =
+    stage === VOTING_STAGE.Vote ? 'Nomination Seconded' : 'Vote Cast';
+
+  return (
+    <Card className={classes.card}>
+      <Box display="flex" flexDirection="column" height="100%">
+        {videoUrl && (
+          <CardMedia
+            className={classes.video}
+            component="video"
+            title={`${firstName} ${lastName} video submission`}
+            src={videoUrl}
+            controls
+          />
+        )}
+        <CardContent className={classes.cardContent}>
+          <Typography variant="h5" component="h2">
+            {firstName} {lastName}
+          </Typography>
+          <Typography variant="overline" color="textSecondary" gutterBottom>
+            {semester} {faculty}
+          </Typography>
+          {writeUp && (
+            <Box overflow="auto">
+              <Typography className={classes.writeUp}>{writeUp}</Typography>
+            </Box>
+          )}
+        </CardContent>
+        <CardActions>
+          <Button
+            size="large"
+            color="primary"
+            onClick={onPrimaryButtonClicked}
+            disabled={isDisabled}
+          >
+            {isDisabled ? primaryButtonDisabledText : primaryButtonActionText}
+          </Button>
+        </CardActions>
+      </Box>
+    </Card>
+  );
+};
+
+export default NomineeCard;
