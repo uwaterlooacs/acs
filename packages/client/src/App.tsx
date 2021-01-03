cf2ba450c24fa687e31ec4c036e95db81b6ea8a5
Christopher Krogh <chris.krogh@outlook.com>
2021-01-03 17:25:12 -0500

    added base pages for voting and events

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -5,6 +5,8 @@ import UserContextProvider from 'context/user/provider';
 import ThemeContextProvider from 'context/theme/provider';
 import AuthPanelProvider from 'context/authPanel/provider';
 import Home from 'pages/Home';
+import Events from 'pages/Events';
+import Voting from 'pages/Voting';
 import Membership from 'pages/Membership';
 import Construction from 'pages/Construction';
 import { ROUTES } from 'utils/constants';
@@ -19,6 +21,8 @@ const App: React.FC = () => {
               <Switch>
                 <Route exact path={ROUTES.LANDING} component={Construction} />
                 <Route exact path={ROUTES.HOME} component={Home} />
+                <Route exact path={ROUTES.EVENTS} component={Events} />
+                <Route exact path={ROUTES.VOTING} component={Voting} />
                 <Route path={ROUTES.MEMBERSHIP} component={Membership} />
               </Switch>
             </BrowserRouter>
---
+++ packages/client/src/pages/Events/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import Page from 'components/Page';
+
+const Events: React.FC = () => {
+  return (
+    <Page>
+      <Box></Box>
+    </Page>
+  );
+};
+
+export default Events;
