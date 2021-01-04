3994f2790695af292d6ccf57d8018febed63a951
Christopher Krogh <chris.krogh@outlook.com>
2021-01-04 17:57:58 -0500

    made route to get all nominations

--- packages/server/src/routers/nomination/types.ts
+++ packages/server/src/routers/nomination/types.ts
@@ -2,4 +2,5 @@ export enum LocalRoutes {
   CREATE_NOMINATION = 'Create a nomination',
   SECOND_NOMINEE = 'Second a candidate for a position',
   VOTE_NOMINEE = 'Vote for a candidate',
+  GET_NOMINATIONS = 'Get nominations by position',
 }
